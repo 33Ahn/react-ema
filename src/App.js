628bc4b8aaff11d5c7077853693a877137c4ddd1
Ahn Shields <ahnshields@gmail.com>
2022-10-12 10:40:55 -0700

    Install react-router-dom, import Router, Route, Routes, configure routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import AllEmployeeComponent from './components/AllEmployeeComponent';
@@ -9,9 +10,17 @@ function App() {
   return (
     <div>      
         <div>
-          <HeaderComponent />
-          <AllEmployeeComponent />
-          <FooterComponent />
+          <Router>
+            <HeaderComponent />
+            <div className="container">
+              <Routes>
+                <Route exact path="/" element = {<AllEmployeeComponent />}></Route>
+                <Route path="/employees" element = {<AllEmployeeComponent />}></Route>
+              </Routes>
+              {/* <AllEmployeeComponent />               */}
+            </div>
+            <FooterComponent />
+          </Router>
         </div>     
     </div>
   );
