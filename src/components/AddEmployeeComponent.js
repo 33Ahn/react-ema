00fd19f605e54141d3456d6d822cdc98d09e2520
Ahn Shields <ahnshields@gmail.com>
2022-10-12 11:36:58 -0700

    Create add employee form, button, define saveEmployee function

--- src/components/AddEmployeeComponent.js
+++ src/components/AddEmployeeComponent.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddEmployeeComponent = () => {
-  return (
-    <div>
-        <h1>Add Employee</h1>
-    </div>
-  )
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const saveEmployee = (e) => {
+        e.preventDefault();
+
+        const employee = {firstName, lastName, email}
+
+        console.log(employee);
+    }
+
+    return (
+        <div className="container">
+            <br></br>
+            <div className="row">
+                <div className="card col-md-6 offset-md-3 offset-md-3">
+                    <h2 className="text-center mt-4">Add Employee</h2>
+                    <div className="card-body">
+                        <form>
+                            <div className="form-group md-3">
+                                <label className="form-label">First Name</label>
+                                <input
+                                    type="text" 
+                                    placeholder="Enter your first name" 
+                                    name="firstName" 
+                                    className="form-control" 
+                                    value={firstName} 
+                                    onChange = {(e) => setFirstName(e.target.value)}>
+                                </input>
+                            </div>
+
+                            <div className="form-group md-3">
+                                <label className="form-label">Last Name</label>
+                                <input
+                                    type="text" 
+                                    placeholder="Enter your last name" 
+                                    name="lastName" 
+                                    className="form-control" 
+                                    value={lastName} 
+                                    onChange = {(e) => setLastName(e.target.value)}>
+                                </input>
+                            </div>
+
+                            <div className="form-group md-3">
+                                <label className="form-label">Email</label>
+                                <input
+                                    type="text" 
+                                    placeholder="Enter your email address" 
+                                    name="email" 
+                                    className="form-control" 
+                                    value={email} 
+                                    onChange = {(e) => setEmail(e.target.value)}>
+                                </input>
+                            </div>
+
+                            <button className="btn btn-success mt-2 mb-2" onClick={(e) => saveEmployee(e)}>Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
+
 
 export default AddEmployeeComponent
